abc2f43e76b98dd6ff2c14eb0dc885b18818054e
MagicBaconDonuts <ricardoalatorre89@gmail.com>
2023-05-20 17:56:51 -0700

    v1.3 Added about me page css
    Added the css to my about me section and made it web responsive.

--- src/Components/sections/about-me/about-me.component.jsx
+++ src/Components/sections/about-me/about-me.component.jsx
@@ -3,17 +3,20 @@ import linkedIn from '../../images/linkedIn.png';
 import './about-me.css';
 
 const AboutMe = forwardRef((props, ref) => {
+
     return(
     <div className='about' ref={ref} >
-        <div className="name">
-            Ricardo Alatorre
-        </div>
         <div className='about-me'>
-            Hello I'm Ricardo Alatorre I became a full-stack Java and React Developer. Pushing to learn more conecepts. I love to code I have a massaive passion for it.
-        </div>
-        <div>
-            View My Linked-In
-            <img src={linkedIn} alt='linked-in icon' className='linked-in-icon'/>
+            <span className='name'>Ricardo Alatorre</span>
+            <span className='text-info text-color '>Hello I'm Ricardo Alatorre I became a full-stack Java and React Developer.
+             Pushing to learn for more. Either trying to learn something new or improve on what I currently know. 
+             Coding is my passion so no matter what i'll always do it. :)</span>
+            <span className='about-me-view'>
+                <span className='text-color'>View My Linked-In </span>
+                <a href='https://www.linkedin.com/in/ricardo-alatorre-a12a78232/'>
+                    <img src={linkedIn} alt='linked-in icon' className='linked-in-icon'/> 
+                </a>
+            </span>
         </div>
     </div>
     )
