06c585dcc9207dd4073a86ed882bef633f591aea
MagicBaconDonuts <ricardoalatorre89@gmail.com>
2023-05-25 23:57:31 -0700

    v1.4 Editing the navbar
    Changed up the css to the entire page but only completed the nav bar so far :)

--- src/Components/Navigation/navigation.component.jsx
+++ src/Components/Navigation/navigation.component.jsx
@@ -1,5 +1,5 @@
 import './navigation.css'
-import logo from '../images/logo.svg';
+import portfolio from '../images/portfolio.png';
 
 const Navigation = ({aboutRef, skillsRef, projectsRef, contactRef}) => {
 
@@ -11,16 +11,18 @@ const Navigation = ({aboutRef, skillsRef, projectsRef, contactRef}) => {
 
     return(
         <div className="nav">
-            <div className="logo-container">
-                <img src={logo} alt="logo" className="logo"/>
-            </div>
-            <div className="container">
-                <ul>
-                    <li onClick={() => scrollToSection(aboutRef)} className="link" >About</li>
-                    <li onClick={() => scrollToSection(skillsRef)} className="link">Skills</li>
-                    <li onClick={() => scrollToSection(projectsRef)} className="link">Projects</li>
-                    <li onClick={() => scrollToSection(contactRef)} className="link">Contact</li>
-                </ul>
+            <div className='nav-items'>
+                <div className="logo-container">
+                    <img src={portfolio} alt="logo" className="logo"/>
+                </div>
+                <div className="container">
+                    <ul>
+                        <li onClick={() => scrollToSection(aboutRef)} className="link" >About</li>
+                        <li onClick={() => scrollToSection(skillsRef)} className="link">Skills</li>
+                        <li onClick={() => scrollToSection(projectsRef)} className="link">Projects</li>
+                        <li onClick={() => scrollToSection(contactRef)} className="link">Contact</li>
+                    </ul>
+                </div>
             </div>
         </div>
     );
