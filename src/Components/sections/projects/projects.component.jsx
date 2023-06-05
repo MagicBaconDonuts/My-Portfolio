1e35372d5e918b66a9c2047dddec33c3f0c8d978
MagicBaconDonuts <ricardoalatorre89@gmail.com>
2023-06-04 19:42:46 -0700

    Creating the project section
    I went ahead and added the project section I went and added extra place holders so in the future I can just implement any future projects that I work on.

--- src/Components/sections/projects/projects.component.jsx
+++ src/Components/sections/projects/projects.component.jsx
@@ -1,4 +1,7 @@
 import { forwardRef } from 'react';
+import usersChat from '../../images/userschatting.png';
+import videoGamReview from '../../images/videogamereview.png'
+import placeholder from '../../images/placeholder.png';
 import './projects.css';
 
 const Projects = forwardRef((props, ref) => {
@@ -6,6 +9,33 @@ const Projects = forwardRef((props, ref) => {
         <div className='projects section-set' ref={ref} >
             <div className="section">
                 Projects
+            </div>
+            <div className='project-list'>
+                <div className='fp1'>
+                    <img src={usersChat} className='format-picture' alt='Two users chatting icon'></img>
+                    <span className='format-text fm1'>Chat System</span>
+                    <span className='format-text fm2'>Java Backend Project</span>
+                </div>
+                <div className='fp1'>
+                    <img src={videoGamReview} className='format-picture' alt='video game review app'></img>
+                    <span className='format-text fm1'>Video Game Review</span>
+                    <span className='format-text fm2'>Full Stack Java Project</span>
+                </div>
+                <div className='fp1'>
+                    <img src={placeholder} className='format-picture' alt='placeholder'></img>
+                    <span className='format-text fm1'>placeholder</span>
+                    <span className='format-text fm2'>placeholder</span>
+                </div>
+                <div className='fp1'>
+                    <img src={placeholder} className='format-picture' alt='video game review app'></img>
+                    <span className='format-text fm1'>placeholder</span>
+                    <span className='format-text fm2'>placeholder</span>
+                </div>
+                <div className='fp1'>
+                    <img src={placeholder} className='format-picture' alt='placeholder'></img>
+                    <span className='format-text fm1'>placeholder</span>
+                    <span className='format-text fm2'>placeholder</span>
+                </div>
             </div>
         </div>
     );
